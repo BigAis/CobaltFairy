5ed1248a2877f553a2516198b6227afb1315e8f4
BigAis <aisbigais@gmail.com>
2025-05-05 14:27:09 +0300

    Settings

--- src/pages/settings/settings.jsx
+++ src/pages/settings/settings.jsx
@@ -9,6 +9,7 @@ import Card from '../../components/Card'
 import Button from '../../components/Button'
 import ButtonGroup from '../../components/ButtonGroup'
 import InputText from '../../components/InputText/InputText'
+import Dropdown from '../../components/Dropdown'
 import Switch from '../../components/Switch'
 import VerificationBadge from '../../components/VerificationBadge'
 import Icon from '../../components/Icon/Icon'
@@ -41,8 +42,8 @@ const Settings = () => {
     const [personalSettings, setPersonalSettings] = useState({
         accountName: '',
         email: '',
-        timezone: 'Timezone',
-        timeformat: 'Timeformat'
+        timezone: null,
+        timeformat: null
     })
     
     // Domain & Identity state
@@ -78,6 +79,83 @@ const Settings = () => {
         lastName: ''
     })
 
+    // Timezone options
+    const timezoneOptions = [
+        { value: "Dateline Standard Time", label: "(UTC-12:00) International Date Line West", offset: -12 },
+        { value: "UTC-11", label: "(UTC-11:00) Coordinated Universal Time-11", offset: -11 },
+        { value: "Hawaiian Standard Time", label: "(UTC-10:00) Hawaii", offset: -10 },
+        { value: "Alaskan Standard Time", label: "(UTC-09:00) Alaska", offset: -8 },
+        { value: "Pacific Standard Time (Mexico)", label: "(UTC-08:00) Baja California", offset: -7 },
+        { value: "Pacific Daylight Time", label: "(UTC-07:00) Pacific Daylight Time (US & Canada)", offset: -7 },
+        { value: "Pacific Standard Time", label: "(UTC-08:00) Pacific Standard Time (US & Canada)", offset: -8 },
+        { value: "US Mountain Standard Time", label: "(UTC-07:00) Arizona", offset: -7 },
+        { value: "Mountain Standard Time (Mexico)", label: "(UTC-07:00) Chihuahua, La Paz, Mazatlan", offset: -6 },
+        { value: "Mountain Standard Time", label: "(UTC-07:00) Mountain Time (US & Canada)", offset: -6 },
+        { value: "Central America Standard Time", label: "(UTC-06:00) Central America", offset: -6 },
+        { value: "Central Standard Time", label: "(UTC-06:00) Central Time (US & Canada)", offset: -5 },
+        { value: "Central Standard Time (Mexico)", label: "(UTC-06:00) Guadalajara, Mexico City, Monterrey", offset: -5 },
+        { value: "Canada Central Standard Time", label: "(UTC-06:00) Saskatchewan", offset: -6 },
+        { value: "SA Pacific Standard Time", label: "(UTC-05:00) Bogota, Lima, Quito", offset: -5 },
+        { value: "Eastern Standard Time", label: "(UTC-05:00) Eastern Time (US & Canada)", offset: -5 },
+        { value: "Eastern Daylight Time", label: "(UTC-04:00) Eastern Daylight Time (US & Canada)", offset: -4 },
+        { value: "US Eastern Standard Time", label: "(UTC-05:00) Indiana (East)", offset: -5 },
+        { value: "Venezuela Standard Time", label: "(UTC-04:30) Caracas", offset: -4.5 },
+        { value: "Paraguay Standard Time", label: "(UTC-04:00) Asuncion", offset: -4 },
+        { value: "Atlantic Standard Time", label: "(UTC-04:00) Atlantic Time (Canada)", offset: -3 },
+        { value: "Central Brazilian Standard Time", label: "(UTC-04:00) Cuiaba", offset: -4 },
+        { value: "SA Western Standard Time", label: "(UTC-04:00) Georgetown, La Paz, Manaus, San Juan", offset: -4 },
+        { value: "Pacific SA Standard Time", label: "(UTC-04:00) Santiago", offset: -4 },
+        { value: "Newfoundland Standard Time", label: "(UTC-03:30) Newfoundland", offset: -2.5 },
+        { value: "E. South America Standard Time", label: "(UTC-03:00) Brasilia", offset: -3 },
+        { value: "Argentina Standard Time", label: "(UTC-03:00) Buenos Aires", offset: -3 },
+        { value: "SA Eastern Standard Time", label: "(UTC-03:00) Cayenne, Fortaleza", offset: -3 },
+        { value: "Greenland Standard Time", label: "(UTC-03:00) Greenland", offset: -3 },
+        { value: "Montevideo Standard Time", label: "(UTC-03:00) Montevideo", offset: -3 },
+        { value: "Bahia Standard Time", label: "(UTC-03:00) Salvador", offset: -3 },
+        { value: "UTC-02", label: "(UTC-02:00) Coordinated Universal Time-02", offset: -2 },
+        { value: "Mid-Atlantic Standard Time", label: "(UTC-02:00) Mid-Atlantic - Old", offset: -1 },
+        { value: "Azores Standard Time", label: "(UTC-01:00) Azores", offset: 0 },
+        { value: "Cape Verde Standard Time", label: "(UTC-01:00) Cape Verde Is.", offset: -1 },
+        { value: "Morocco Standard Time", label: "(UTC) Casablanca", offset: 1 },
+        { value: "UTC", label: "(UTC) Coordinated Universal Time", offset: 0 },
+        { value: "GMT Standard Time", label: "(UTC) Edinburgh, London", offset: 0 },
+        { value: "British Summer Time", label: "(UTC+01:00) Edinburgh, London", offset: 1 },
+        { value: "GMT Standard Time", label: "(UTC) Dublin, Lisbon", offset: 1 },
+        { value: "Greenwich Standard Time", label: "(UTC) Monrovia, Reykjavik", offset: 0 },
+        { value: "W. Europe Standard Time", label: "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna", offset: 2 },
+        { value: "Central Europe Standard Time", label: "(UTC+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague", offset: 2 },
+        { value: "Romance Standard Time", label: "(UTC+01:00) Brussels, Copenhagen, Madrid, Paris", offset: 2 },
+        { value: "Central European Standard Time", label: "(UTC+01:00) Sarajevo, Skopje, Warsaw, Zagreb", offset: 2 },
+        { value: "W. Central Africa Standard Time", label: "(UTC+01:00) West Central Africa", offset: 1 },
+        { value: "Namibia Standard Time", label: "(UTC+01:00) Windhoek", offset: 1 },
+        { value: "GTB Standard Time", label: "(UTC+02:00) Athens, Bucharest", offset: 3 },
+        { value: "Middle East Standard Time", label: "(UTC+02:00) Beirut", offset: 3 },
+        { value: "Egypt Standard Time", label: "(UTC+02:00) Cairo", offset: 2 },
+        { value: "Syria Standard Time", label: "(UTC+02:00) Damascus", offset: 3 },
+        { value: "E. Europe Standard Time", label: "(UTC+02:00) E. Europe", offset: 3 },
+        { value: "South Africa Standard Time", label: "(UTC+02:00) Harare, Pretoria", offset: 2 },
+        { value: "FLE Standard Time", label: "(UTC+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius", offset: 3 },
+        { value: "Turkey Standard Time", label: "(UTC+03:00) Istanbul", offset: 3 },
+        { value: "Israel Standard Time", label: "(UTC+02:00) Jerusalem", offset: 3 },
+        { value: "Libya Standard Time", label: "(UTC+02:00) Tripoli", offset: 2 },
+        { value: "Jordan Standard Time", label: "(UTC+03:00) Amman", offset: 3 },
+        { value: "Arabic Standard Time", label: "(UTC+03:00) Baghdad", offset: 3 },
+        { value: "Kaliningrad Standard Time", label: "(UTC+02:00) Kaliningrad", offset: 3 },
+        { value: "Arab Standard Time", label: "(UTC+03:00) Kuwait, Riyadh", offset: 3 },
+        { value: "E. Africa Standard Time", label: "(UTC+03:00) Nairobi", offset: 3 },
+        { value: "Moscow Standard Time", label: "(UTC+03:00) Moscow, St. Petersburg, Volgograd, Minsk", offset: 3 },
+        { value: "Samara Time", label: "(UTC+04:00) Samara, Ulyanovsk, Saratov", offset: 4 },
+        { value: "Iran Standard Time", label: "(UTC+03:30) Tehran", offset: 4.5 },
+        { value: "Arabian Standard Time", label: "(UTC+04:00) Abu Dhabi, Muscat", offset: 4 }
+    ];
+
+    // Date format options
+    const dateFormatOptions = [
+        { value: "yyyy-mm-dd", label: "yyyy-mm-dd" },
+        { value: "dd/mm/yyyy", label: "dd/mm/yyyy" },
+        { value: "mm/dd/yyyy", label: "mm/dd/yyyy" }
+    ];
+
     useEffect(() => {
         // Update active section when route changes
         if (section && sections[section]) {
@@ -100,8 +178,8 @@ const Settings = () => {
             setPersonalSettings({
                 accountName: account?.name || 'Account Name',
                 email: user?.user?.email || 'johndoe@gmail.com',
-                timezone: 'Timezone',
-                timeformat: 'Timeformat'
+                timezone: timezoneOptions.find(tz => tz.label === "(UTC+02:00) Athens, Bucharest") || timezoneOptions[0],
+                timeformat: dateFormatOptions[1] // Default to dd/mm/yyyy
             })
             
             setDomainSettings({
@@ -180,6 +258,22 @@ const Settings = () => {
         setActiveSection(value)
     }
 
+    // Handle timezone selection
+    const handleTimezoneSelect = (option) => {
+        setPersonalSettings({
+            ...personalSettings,
+            timezone: option
+        })
+    }
+
+    // Handle date format selection
+    const handleDateFormatSelect = (option) => {
+        setPersonalSettings({
+            ...personalSettings,
+            timeformat: option
+        })
+    }
+
     const renderTabContent = () => {
         switch (activeSection) {
             case 'details':
@@ -204,21 +298,23 @@ const Settings = () => {
                         <div className="dropdown-section">
                             <div className="dropdown-row">
                                 <div className="dropdown-item">
-                                    <InputText
-                                        label="Timezone"
-                                        value={personalSettings.timezone}
-                                        onChange={(e) => {}} // This would be a dropdown in the real UI
-                                    />
-                                    <div className="dropdown-arrow">▼</div>
+                                    <Dropdown
+                                        options={timezoneOptions}
+                                        selectedValue={personalSettings.timezone}
+                                        onOptionSelect={handleTimezoneSelect}
+                                    >
+                                        Timezone
+                                    </Dropdown>
                                 </div>
                                 
                                 <div className="dropdown-item">
-                                    <InputText
-                                        label="Timeformat"
-                                        value={personalSettings.timeformat}
-                                        onChange={(e) => {}} // This would be a dropdown in the real UI
-                                    />
-                                    <div className="dropdown-arrow">▼</div>
+                                    <Dropdown
+                                        options={dateFormatOptions}
+                                        selectedValue={personalSettings.timeformat}
+                                        onOptionSelect={handleDateFormatSelect}
+                                    >
+                                        Date Format
+                                    </Dropdown>
                                 </div>
                             </div>
                         </div>
