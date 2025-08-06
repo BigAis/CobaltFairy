2a62c56aead9a900ae526825a95fb7fc180e89ba
BigAis <aisbigais@gmail.com>
2025-08-06 12:33:34 +0300

    Notification Remove Clear All

--- src/components/NotificationContainer/NotificationContainer.jsx
+++ src/components/NotificationContainer/NotificationContainer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { useAccount } from '../../context/AccountContext'
 import NotificationBar from '../NotificationBar/NotificationBar'
 import './NotificationContainer.scss'
-import Button from '../Button'
-import Icon from '../Icon/Icon'
 
 const NotificationContainer = () => {
-  const { notifications, dismissNotification, clearAllNotifications } = useAccount()
+  const { notifications, dismissNotification } = useAccount()
 
   if (notifications.length === 0) {
     return null
@@ -14,16 +12,6 @@ const NotificationContainer = () => {
 
   return (
     <div className="notification-container">
-      <div className="notification-controls">
-        <Button 
-          type="secondary" 
-          className="clear-all-btn"
-          onClick={clearAllNotifications}
-        >
-          Clear All
-        </Button>
-      </div>
-      
       {notifications.map((notification) => (
         <NotificationBar
           key={notification.id}
