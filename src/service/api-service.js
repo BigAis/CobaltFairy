cf324b6f234aa8d3b13c5bc9350afd805f76a56d
ctzoum <xristos.jum@gmail.com>
2025-09-02 16:58:48 +0300

    patched version

--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { jwtDecode } from 'jwt-decode'
-const APP_VERSION = '0.7.7' 
+const APP_VERSION = '0.7.8' 
  const BASE_URL = 'https://fairymail.cobaltfairy.com/api'
 // const BASE_URL = 'http://192.168.3.77:1337/api'
 //const BASE_URL = 'http://localhost:1337/api'
